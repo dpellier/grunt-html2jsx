8a872328be118ef5d939071a73812f4e2e9ebfcd
Damien Pellier <dpellier@leadformance.com>
2015-03-11 15:55:42 +0100

    feat(app): add React module require call

--- tasks/html2jsx.js
+++ tasks/html2jsx.js
@@ -53,8 +53,10 @@ module.exports = function(grunt) {
     grunt.registerMultiTask('html2jsx', 'Convert HTML files to JSX', function() {
         // Iterate over all specified file groups.
         this.files.forEach(function(f) {
+            var src = 'var React = require(\'react\');\n';
+
             // Concat specified files.
-            var src = f.src.filter(function(filepath) {
+            src += f.src.filter(function(filepath) {
                 // Warn on and remove invalid source files (if nonull was set).
                 if (!grunt.file.exists(filepath)) {
                     grunt.log.warn('Source file "' + filepath + '" not found.');
--- test/expected/defaultOptions.jsx
+++ test/expected/defaultOptions.jsx
@@ -1,3 +1,4 @@
+var React = require('react');
 var File1Html = React.createClass({
   render: function() {
     return (
